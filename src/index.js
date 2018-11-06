3ac58e1b68c5a98c2e3e8644ecf37d1947b7ac32
Austin Sauer <inventorguy2235@hotmail.com>
2018-11-06 14:45:19 -0600

    finished the assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-// TODO import solution function from lib.js
+import lastDayIsFriday from './lib.js';
 
-// TODO add some example test cases and log their results to the console
+// returns a number
+console.log(typeof(lastDayIsFriday(1901, 2000)) === 'number',
+
+//returns 171 when between 1901 and 2000
+console.log(lastDayIsFriday(1901, 2000)) === 171,
+
+//returns 200 when between 1991 and 201
+console.log(lastDayIsFriday(1901, 2017)) === 200,
+
+//returns 1 when 1991 
+console.log(lastDayIsFriday(1991))=== 1)
